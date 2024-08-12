3a52b0c6259abb76ce098a228e62b8bbca6f08f0
mcrnkovic <mcrnkovic@autozubak.hr>
2024-08-12 20:28:07 +0200

    initialized spring project

---
+++ vhs-lab-mihovil/src/main/java/com/example/vhs_lab_mihovil/VhsLabMihovilApplication.java
@@ -0,0 +1,13 @@
+package com.example.vhs_lab_mihovil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsLabMihovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhsLabMihovilApplication.class, args);
+	}
+
+}
---
+++ vhs-lab-mihovil/src/test/java/com/example/vhs_lab_mihovil/VhsLabMihovilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vhs_lab_mihovil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsLabMihovilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
