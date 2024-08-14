1b7fa60efb54c6a92db403badb2709fa28d2ca7d
mcrnkovic <mcrnkovic@autozubak.hr>
2024-08-14 22:28:23 +0200

    toModel mapping with from userId and vhsId

--- vhs-lab-mihovil/src/main/java/com/example/vhs_lab_mihovil/mapper/RentalMapper.java
+++ vhs-lab-mihovil/src/main/java/com/example/vhs_lab_mihovil/mapper/RentalMapper.java
@@ -9,13 +9,17 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = VhsMapper.class)
 public interface RentalMapper extends GenericMapper<Rental, RentalDto> {
     RentalMapper MAPPER = Mappers.getMapper(RentalMapper.class);
 
     @Mapping(source = "user", target = "userId", qualifiedByName = "getUserId")
     @Mapping(source = "vhs", target = "vhsId", qualifiedByName = "getVhsId")
     RentalDto toDto(Rental rental);
+
+    @Mapping(source = "userId", target = "user", qualifiedByName = "getUser")
+    @Mapping(source = "vhsId", target = "vhs", qualifiedByName = "getVhs")
+    Rental toModel(RentalDto rentalDto);
 
     @Named("getUserId")
     static Integer getUserId(User user) {
@@ -25,5 +29,19 @@ public interface RentalMapper extends GenericMapper<Rental, RentalDto> {
     @Named("getVhsId")
     static Integer getVhsId(Vhs vhs) {
         return vhs.getId();
+    }
+
+    @Named("getUser")
+    static User getUser(Integer userId) {
+        User user = new User();
+        user.setId(userId);
+        return user;
+    }
+
+    @Named("getVhs")
+    static Vhs getVhs(Integer vhsId) {
+        Vhs vhs = new Vhs();
+        vhs.setId(vhsId);
+        return vhs;
     }
 }
