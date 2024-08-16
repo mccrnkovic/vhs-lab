77a11ec3d55a8d2a1cf9314e40b8987ba32169f7
mcrnkovic <mcrnkovic@autozubak.hr>
2024-08-16 16:30:54 +0200

    vhs and user initial data

--- vhs-lab-mihovil/src/main/resources/data.sql
+++ vhs-lab-mihovil/src/main/resources/data.sql
@@ -8,3 +8,10 @@ INSERT INTO VHS(TITLE, PUBLISHER, YEAR) VALUES ('The Lion King', 'Disney', 1994)
 INSERT INTO VHS(TITLE, PUBLISHER, YEAR) VALUES ('The Lion King', 'Disney', 1994);
 INSERT INTO VHS(TITLE, PUBLISHER, YEAR) VALUES ('Titanic', 'Paramount Pictures', 1997);
 INSERT INTO VHS(TITLE, PUBLISHER, YEAR) VALUES ('Jurassic Park', 'Universal Studios', 1993);
+
+--User data
+INSERT INTO APP_USER(NAME, SURNAME, USERNAME, EMAIL) VALUES ('John', 'Doe', 'jdoe', 'jdoe@gmail.com');
+INSERT INTO APP_USER(NAME, SURNAME, USERNAME, EMAIL) VALUES ('Ned', 'Harret', 'nharret', 'nharret@gmail.com');
+INSERT INTO APP_USER(NAME, SURNAME, USERNAME, EMAIL) VALUES ('Darell', 'Feron', 'dferon', 'dferon@gmail.com');
+INSERT INTO APP_USER(NAME, SURNAME, USERNAME, EMAIL) VALUES ('Ana', 'Wilcher', 'awilcher', 'awilcher@gmail.com');
+
