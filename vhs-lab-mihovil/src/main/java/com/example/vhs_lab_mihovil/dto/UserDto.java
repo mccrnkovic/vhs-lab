5f3ab6eb1bfcb52b938b15ab7f3a6fa8a3388406
mcrnkovic <mcrnkovic@autozubak.hr>
2024-08-20 20:18:51 +0200

    userDto validation

--- vhs-lab-mihovil/src/main/java/com/example/vhs_lab_mihovil/dto/UserDto.java
+++ vhs-lab-mihovil/src/main/java/com/example/vhs_lab_mihovil/dto/UserDto.java
@@ -1,13 +1,18 @@
 package com.example.vhs_lab_mihovil.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserDto {
     Integer id;
+    @NotNull(message = "{notNullMsg}")
     String name;
+    @NotNull(message = "{notNullMsg}")
     String surname;
+    @NotNull(message = "{notNullMsg}")
     String username;
+    @NotNull(message = "{notNullMsg}")
     String email;
     String phoneNumber;
 }
